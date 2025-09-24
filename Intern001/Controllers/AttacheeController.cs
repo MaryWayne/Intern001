b69acfcd8a81193891f6e4a983e366c9aa150937
Mary Wainaina <waynmary9@gmail.com>
2025-09-24 15:29:08 +0300

    added controller

---
+++ Intern001/Controllers/AttacheeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Intern001.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AttacheeController : ControllerBase
+    {
+        // GET: api/<AttacheeController>    
+    }
+}
---
+++ Intern001/Controllers/InternController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Intern001.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InternController : ControllerBase
+    {
+        // GET: api/<InternController>
+    }
+}
