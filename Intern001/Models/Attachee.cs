9b75d4e5301ce3376efc43804d7a06be84d41ff8
Mary Wainaina <waynmary9@gmail.com>
2025-09-24 15:20:49 +0300

    added models

---
+++ Intern001/Models/Attachee.cs
@@ -0,0 +1,7 @@
+﻿namespace Intern001.Models
+{
+    public class Attachee
+    {
+        // Properties and methods for the Attachee class
+    }
+}
---
+++ Intern001/Models/Intern.cs
@@ -0,0 +1,7 @@
+﻿namespace Intern001.Models
+{
+    public class Intern
+    {
+        //
+    }
+}
